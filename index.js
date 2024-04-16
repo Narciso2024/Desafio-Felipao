646203edaa3a6506e902118982b3547c1b95a5ae
Narciso2024 <jnarciso@gmail.com>
2024-04-16 19:55:37 -0300

    Desafio Felipao Dio

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log('TESTE');
+console.log('ate que fim');
+let nome = 'Joao';
+let qtde = 5000;
+let nivel = 'a qtde informada está fora de parametro!';
+
+if (qtde < 1000) {
+  console.log('O Heroi ' + nome + ' esta no nivel Ferro !');
+}
+if (qtde > 1001 && qtde < 2000) {
+  console.log('O Heroi ' + nome + ' esta no nivel Bronze !');
+}
+if (qtde > 2001 && qtde < 5000) {
+  console.log('O Heroi ' + nome + ' esta no nivel Prata !');
+}
+if (qtde > 5001 && qtde < 7000) {
+  console.log('O Heroi ' + nome + ' esta no nivel Ouro !');
+}
+if (qtde > 7001 && qtde < 8000) {
+  console.log('O Heroi ' + nome + ' esta no nivel Platina !');
+}
+if (qtde > 8001 && qtde < 9000) {
+  console.log('O Heroi ' + nome + ' esta no nivel Ascendente !');
+}
+if (qtde > 9001 && qtde < 10000) {
+  console.log('O Heroi ' + nome + ' esta no nivel Imortal !');
+}
+if (qtde >= 10001) {
+  console.log('O Heroi ' + nome + ' esta no nivel Radiante !');
+}
+console.log('O Heroi ' + nome + ' esta no nivel Radiante !');
