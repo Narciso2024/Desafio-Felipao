c37817f9274aed18259b98df1d3d3ee527aad473
Narciso2024 <jnarciso@gmail.com>
2024-04-16 21:11:00 -0300

    atualizacao desafio

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-console.log('TESTE');
-console.log('ate que fim');
 let nome = 'Joao';
-let qtde = 5000;
-let nivel = 'a qtde informada está fora de parametro!';
+let qtde = 9000;
+let nivel = 'fora de parametro!';
 
 if (qtde < 1000) {
-  console.log('O Heroi ' + nome + ' esta no nivel Ferro !');
+  nivel = 'Ferro !';
 }
 if (qtde > 1001 && qtde < 2000) {
-  console.log('O Heroi ' + nome + ' esta no nivel Bronze !');
+  nivel = 'Bronze !';
 }
 if (qtde > 2001 && qtde < 5000) {
-  console.log('O Heroi ' + nome + ' esta no nivel Prata !');
+  nivel = 'Prata !';
 }
 if (qtde > 5001 && qtde < 7000) {
-  console.log('O Heroi ' + nome + ' esta no nivel Ouro !');
+  nivel = 'Ouro !';
 }
 if (qtde > 7001 && qtde < 8000) {
-  console.log('O Heroi ' + nome + ' esta no nivel Platina !');
+  nivel = 'Platina !';
 }
 if (qtde > 8001 && qtde < 9000) {
-  console.log('O Heroi ' + nome + ' esta no nivel Ascendente !');
+  nivel = 'Ascendente !';
 }
 if (qtde > 9001 && qtde < 10000) {
-  console.log('O Heroi ' + nome + ' esta no nivel Imortal !');
+  nivel = 'Imortal !';
 }
 if (qtde >= 10001) {
-  console.log('O Heroi ' + nome + ' esta no nivel Radiante !');
+  nivel = 'Radiante !';
 }
-console.log('O Heroi ' + nome + ' esta no nivel Radiante !');
+console.log(
+  'O Heroi ' +
+    nome +
+    ' obteve ' +
+    qtde +
+    ' ponto(s) ' +
+    'esta no nivel ' +
+    nivel
+);
